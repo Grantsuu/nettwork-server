c02ddab3b2f90ed29f0a1c9662d1c94fba112fc8
Grant Sherman <tnarg122@gmail.com>
2023-03-10 00:52:42 -0500

    gin example

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
-func main(){
+type Ball struct {
+	Name string
+}
+
+func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
+
+	balls := []Ball{
+		Ball{"bill"},
+		Ball{"ted"},
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+			"balls":   balls,
 		})
 	})
+
 	r.Run()
 }
