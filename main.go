832ad38d329e46c1c466800b077145b94de7f01d
Grant Sherman <tnarg122@gmail.com>
2023-03-09 22:23:23 -0500

    gin

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main(){
-	fmt.Println("Hello world!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
